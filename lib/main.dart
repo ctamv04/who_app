a36fa0c78786b995a07c2b65ee5d4ad86fe23b5d
Christos Tamvakas <C.Tamvakas@student.tudelft.nl>
2025-01-10 07:29:51 +0200

    Added Google Maps API to project

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'models/form.dart' as form_model;
 import 'models/page.dart' as page_model;
 import 'models/selection.dart';
 import 'models/text.dart' as text_model;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 Future<void> main() async {
 
@@ -23,11 +24,49 @@ Future<void> main() async {
   var snap = await db.collection("forms").get();
   var forms = snap.docs.map((x) => form_model.Form.fromJson(x.data())).toList();
 
-  runApp(MaterialApp(
-      title: 'WHO Form Manager',
-      routes: {
-        '/': (context) => HomePage(db: db),
-      },
-      initialRoute: '/',
-  ));
+  // runApp(MaterialApp(
+  //     title: 'WHO Form Manager',
+  //     routes: {
+  //       '/': (context) => HomePage(db: db),
+  //     },
+  //     initialRoute: '/',
+  // ));
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-33.86, 151.20);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
