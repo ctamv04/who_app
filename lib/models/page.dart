f4056512f15924ed3bc6355861543dc266d6cfd7
Christos Tamvakas <C.Tamvakas@student.tudelft.nl>
2025-01-13 06:11:02 +0200

    Revert "Updated models"
    This reverts commit 802ef06682698fb8fe3cd32b88cb2e931f1ede3a.

--- lib/models/page.dart
+++ lib/models/page.dart
@@ -1,28 +1,41 @@
-import 'field_page.dart';
-import 'location_page.dart';
+import 'dart:convert';
 
-abstract class Page {
+import 'form_element.dart';
+
+class Page {
 
   String _title;
   String _description;
+  int _pageNumber;
+  Map<int, FormElement>? _elements;
 
   Page({
     required String title,
+    required int pageNumber,
     String? description,
-  }) : _title = title, _description = description ?? "";
+    Map<int, FormElement>? elements
+  }) : _title = title, _pageNumber = pageNumber, _description = description ?? "", _elements = elements ?? {};
 
   factory Page.fromJson(Map<String, dynamic> json) {
 
-    var type = (json['type'] ?? "") as String;
-    if(type == "location"){
-      return LocationPage.fromJson(json);
-    }else{
-      return FieldPage.fromJson(json);
-    }
+    return Page(
+      title: json['title'] as String,
+      pageNumber: json['page_number'] as int,
+      description: json['description'] as String,
+      elements: (json['elements'] as Map<String, dynamic>).map((k,v) => MapEntry(int.parse(k), FormElement.fromJson(v)))
+    );
   }
 
-  Map<String, dynamic> toJson();
+  Map<String, dynamic> toJson() =>
+      {
+        'title': _title,
+        'page_number': _pageNumber,
+        'description': _description,
+        'elements': _elements?.map((k,v) => MapEntry(k.toString(), v.toJson()))
+      };
 
   String get title => _title;
+  int get pageNumber => _pageNumber;
   String get description => _description;
+  Map<int, FormElement>? get elements => _elements;
 }
